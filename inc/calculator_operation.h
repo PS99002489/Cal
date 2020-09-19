80257d7b8c8aa572baaf076657ba28f583c144a2
PS99002489 <71481217+PS99002489@users.noreply.github.com>
2020-09-19 11:24:47 +0530

    Update calculator_operation.h

--- inc/calculator_operation.h
+++ inc/calculator_operation.h
@@ -9,12 +9,12 @@
 
 int add(int operand1, int operand2);
 
-int subtract(int operand1, int operand2);
+int sub(int operand1, int operand2);
 
 
-int multiply(int operand1, int operand2);
+int mult(int operand1, int operand2);
 
 
-int divide(int operand1, int operand2);
+int div(int operand1, int operand2);
 
 #endif  /* #define __CALCULATOR_OPERATIONS_H__ */
